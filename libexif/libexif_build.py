ce0197078644f99ce7ed9a3edbb60d943a106882
JT <dieselmachine@gmail.com>
2025-05-22 19:44:34 +0000

    fixes for py2

--- libexif/libexif_build.py
+++ libexif/libexif_build.py
@@ -13,14 +13,14 @@ PROJECT_PATH = Path(__file__).parent.parent
 PACKAGE_ABS_PATH = Path(__file__).parent
 PACKAGE_REL_PATH = PACKAGE_ABS_PATH.relative_to(PROJECT_PATH)
 
-with open(PACKAGE_ABS_PATH / 'c-src' / 'libexif.c', 'r') as f:
+with open(str(PACKAGE_ABS_PATH / 'c-src' / 'libexif.c'), 'r') as f:
     ffibuilder.set_source(
         module_name="_libexif",
         source=f.read(),
         libraries=["exif"],
     )
 
-with open(PACKAGE_ABS_PATH / 'c-src' / 'cdef.c', 'r') as f:
+with open(str(PACKAGE_ABS_PATH / 'c-src' / 'cdef.c'), 'r') as f:
     ffibuilder.cdef(f.read())
 
 
